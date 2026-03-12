1493a88b86780ecb6fd2169d945d835d097de707
johnkrause1 <jkkrause@gmail.com>
2026-03-12 08:01:27 -0700

    reverting back to phone only for mobile

--- src/components/CTAMiniSection.tsx
+++ src/components/CTAMiniSection.tsx
@@ -62,7 +62,7 @@ const CTAMiniSection = () => {
               </a>
               <a
                 href="/connect"
-                className="group text-secondary-foreground cursor-pointer"
+                className="group text-secondary-foreground cursor-pointer block max-[640px]:hidden"
                 aria-label="Share your details and we will contact you for an appointment."
               >
                 <div className="flex flex-col items-center text-center gap-3">
