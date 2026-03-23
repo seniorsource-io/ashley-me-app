9c355b8bd45e3d43e11dbfe7d9b2fbe3a662ce0d
johnkrause1 <jkkrause@gmail.com>
2026-03-23 06:44:18 -1000

    disable mobile hot actions

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -3,6 +3,14 @@ import Navbar from "@/components/Navbar";
 import CommunityContact from "@/components/CommunityContact";
 import CommunityJoinUs from "@/components/CommunityJoinUs";
 import Footer from "@/components/Footer";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  formatDetection: {
+    address: false,
+    email: false
+  },
+}
 
 export default function CommunityForm() {
     return (
